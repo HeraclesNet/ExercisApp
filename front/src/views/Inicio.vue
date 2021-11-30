648aa5424edb753485b6b3435283bba815689e16
MIGUEL CURREA <mcurream@unal.edu.co>
2021-11-30 16:47:21 -0500

    New 2.0

---
+++ front/src/views/Inicio.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <div id = "logo">
+      <h1>Heracles</h1>
+      <h2>Retis</h2>
+      <p> Aqui iria mi slogan si tuviera uno UwU </p>
+    </div>
+    <LoginBox/>
+  </div>
+</template>
+
+<script>
+import LoginBox from '@/components/LoginBox.vue'
+export default {
+  name: 'Inicio',
+  components: {
+    LoginBox
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Road+Rage&display=swap');
+.home {
+  float: left;
+  display: inline;
+  text-align: center
+}
+
+#logo {
+  margin: 10rem 0;
+  color : #FFFBF4;
+}
+
+#logo h1 {
+  margin: 0;
+  padding: 0.1rem;
+  display: inline;
+  font-weight: 500;
+  letter-spacing: 0.2rem;
+  font-family: 'Road Rage', cursive;
+  font-size: 8rem;
+}
+
+#logo h2 {
+  margin: 0;
+  padding: 0.1rem;
+  display: inline;
+  font-size: 0.8rem;
+}
+
+#logo p{
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 25px;
+}
+
+</style>
---
+++ front/src/views/Registro.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Registro">
+    <h1>Registro</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Registro',
+  components: {
+  }
+}
+</script>
